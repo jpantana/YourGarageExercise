a5ddc44d3de13d134ce16b482a60b96287a778e7
jpantana <josh.pantana@gmail.com>
2019-09-09 08:25:07 -0500

    invoking methods

--- YourGarageExercise/Program.cs
+++ YourGarageExercise/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using YourGarageExercise.Vehicles;
 using YourGarageExercise.Vehicles.Cars;
+using YourGarageExercise.Vehicles.Watercrafts;
 
 namespace YourGarageExercise
 {
@@ -9,8 +10,13 @@ namespace YourGarageExercise
         static void Main(string[] args)
         {
             var newSportsCar = new SportsCar(FuelStatus.Empty, "Blue", 4);
-
             newSportsCar.Drive();
+            newSportsCar.Refuel();
+            newSportsCar.Brake();
+
+            var newMotorBoat = new MotorBoat(FuelStatus.Full, "Gray", 15);
+            newMotorBoat.Drive();
+            newMotorBoat.Refuel();
         }
     }
 }
--- YourGarageExercise/Vehicles/Cars/SportsCar.cs
+++ YourGarageExercise/Vehicles/Cars/SportsCar.cs
@@ -14,17 +14,30 @@ namespace YourGarageExercise.Vehicles.Cars
         }
         public override void Drive()
         {
+            if (FuelAtPresent == FuelStatus.Empty)
+            {
+                Console.WriteLine($"Your running on {FuelAtPresent}, and need to refuel before you drive.");
+                return;
+            }
             Console.WriteLine("Driving my Sportscar");
         }
 
         public override void Brake()
         {
-            Console.WriteLine("Slowing down in my sportscar");
+
+            Console.WriteLine($"Your car can hold {PassengerCapacity}. How many people (including yourself) are in the vehicle?");
+            var howManyRiding = int.Parse(Console.ReadLine());
+            Console.WriteLine($"There are {howManyRiding} people in my Sportscar");
+            if (PassengerCapacity == howManyRiding)
+            {
+                Console.WriteLine("You need to slow down, bc you've got a full vehicle.");
+            }
+
         }
 
         public override void Refuel()
         {
-            Console.WriteLine("Need to refuel my sportscar");
+            Console.WriteLine(FuelAtPresent);
         }
     }
 }
